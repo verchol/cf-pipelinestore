1878cdcc21830403131d9f5a4f6cfa9dce91b982
verchol <verchol@gmail.com>
2019-03-23 00:46:55 +0200

    fix

--- .codefresh/clone.sh
+++ .codefresh/clone.sh
@@ -1,9 +1,3 @@
-
-    echo clone started
-    if [ ! -d "/codefresh/volume/$REPO_NAME" ]; then
-           git clone https://${{GIT_TOKEN}}@github.com/${{REPO_OWNER}}/${{REPO_NAME}}.git
-    fi
-    cd  /codefresh/volume/$REPO_NAME
-    mv submit.json submit.new.json
-    git checkout HEAD~1 submit.json 
-    mv submit.json > submit.old.json
+if [ ! -d "/codefresh/volume/cf-pipelinestore" ]; then
+           git clone https://${{GIT_TOKEN}}@github.com/verchol/cf-pipelinestore.git
+fi
