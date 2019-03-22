b2206982ee6b8a1a757e50c193ef9c107eacbf53
verchol <verchol@gmail.com>
2019-03-23 01:04:50 +0200

    Fix

--- .codefresh/submit.sh
+++ .codefresh/submit.sh
@@ -1,9 +1,14 @@
 
-    echo clone started
-    if [ ! -d "/codefresh/volume/$REPO_NAME" ]; then
-           git clone https://${{GIT_TOKEN}}@github.com/${{REPO_OWNER}}/${{REPO_NAME}}.git
+    echo processing codefresh-io/plugins/submit.json 
+    #export REPO_OWNER=codefresh-io
+    #export REPO_NAME=plugins
+ 
+    if [ ! -d "$PWD/$REPO_NAME" ]; then
+           echo cloning ...
+           git clone https://$GIT_TOKEN@github.com/$REPO_OWNER/$REPO_NAME.git
     fi
-    cd  /codefresh/volume/$REPO_NAME
+    cd  $PWD/$REPO_NAME
+    echo $PWD/$REPO_NAME
     mv submit.json submit.new.json
     git checkout HEAD~1 submit.json 
-    mv submit.json > submit.old.json
+    mv submit.json submit.old.json
