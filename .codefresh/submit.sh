0348842b16556de8f72ebd46bc2ae175ae574bb8
Oleg Verhovsky <verchol@gmail.com>
2019-04-01 08:44:44 +0300

    Update submit.sh

--- .codefresh/submit.sh
+++ .codefresh/submit.sh
@@ -8,7 +8,8 @@
            git clone https://$GIT_TOKEN@github.com/$REPO_OWNER/$REPO_NAME.git
     fi
     cd  $PWD/$REPO_NAME
-    echo $PWD
+    echo $PWD 
+    ls .
     mv submit.json submit.new.json
     git checkout HEAD~1 submit.json 
     mv submit.json submit.old.json
