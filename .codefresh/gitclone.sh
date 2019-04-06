aae889de38db532fabe166e459cbf7dd07cc9b2b
Oleg Verhovsky <verchol@gmail.com>
2019-04-06 03:46:01 +0300

    Update gitclone.sh

--- .codefresh/gitclone.sh
+++ .codefresh/gitclone.sh
@@ -1,4 +1,9 @@
+export REPO_BRANCH=new-pipeline
 if [ ! -d "$REPO_NAME" ]; then
            echo cloning ...
            git clone https://$GIT_TOKEN@github.com/$REPO_OWNER/$REPO_NAME.git
 fi
+
+cd ./$REPO_NAME
+git checkout $REPO_BRANCH 
+
